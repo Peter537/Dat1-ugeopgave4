232f99c0333b60c515748c55386ce2f1011e54c1
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-09-29 12:32:54 +0200

    ArrayList

--- Torsdagsopgaver 4/src/task4/Main.java
+++ Torsdagsopgaver 4/src/task4/Main.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> actions = new ArrayList<>();
-        actions.add("Start game");
-        actions.add("Resume game");
-        actions.add("Pause game");
-        actions.add("End game");
+        ArrayList<String> actions = new ArrayList<>() { {
+            add("Start game");
+            add("Resume game");
+            add("Pause game");
+            add("End game");
+        }};
         System.out.println(actions.get(2)); // expected output: "Pause game"
 
         Menu menu = new Menu(actions);
