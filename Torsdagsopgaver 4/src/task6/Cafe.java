dc487d7675dccfd0b5b123dcc65fdab9a19e3c1e
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-09-29 09:34:13 +0200

    Task6

---
+++ Torsdagsopgaver 4/src/task6/Cafe.java
@@ -0,0 +1,43 @@
+package task6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Cafe {
+
+    private ArrayList<String> coffeeMenu;
+
+    public Cafe() {
+        this.coffeeMenu = new ArrayList<>();
+    }
+
+
+
+    public ArrayList<String> getCoffeeMenu() {
+        return coffeeMenu;
+    }
+
+    public void loadMenuData() {
+        File file = new File("src\\task6\\coffees.txt");
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                coffeeMenu.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cafe{" +
+                "coffeeMenu=" + coffeeMenu +
+                '}';
+    }
+}
---
+++ Torsdagsopgaver 4/src/task6/Main.java
@@ -0,0 +1,15 @@
+package task6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cafe cafe = new Cafe();
+        cafe.loadMenuData();
+        for (String coffee : cafe.getCoffeeMenu()) {
+            System.out.println(coffee);
+        }
+
+    }
+
+
+}
